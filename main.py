c3018f33090df3fbc6b20e5424ca2a0b32f2c543
pavlov_i_a <pavlov2.ia@edu.spbstu.ru>
2024-11-07 21:10:53 +0300

    Stream Archive and Download

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse
 import os
+import zipstream
+import random
 
 import uvicorn
 
 app = FastAPI()
 
-dir = "Files"
+dir = "Files\\"
 
 @app.get("/count")
 async def Count():
     return str(len(os.listdir(dir)))
 
+
 @app.get("/download")
 async def download(num : int = 0):
-    if num < 0 or num >= len(os.listdir(dir)):
+    if num == 0 or num > len(os.listdir(dir)):
         return "Укажите другое количество файлов"
-    return FileResponse(f"{dir}\\{os.listdir(dir)[num]}", filename=os.listdir(dir)[num])
 
+    dirlist = os.listdir(dir)
+    randomnumbers = list()
+    for i in range(num):
+        randomnumbers.append(random.choice(dirlist))
+        dirlist.remove(randomnumbers[len(randomnumbers) - 1])
+    z = zipstream.ZipFile()
+    for data in randomnumbers:
+        z.write(f"Files\\{data}")
+
+    return StreamingResponse(z)
 
 
 if __name__ == '__main__':
