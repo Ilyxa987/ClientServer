3ee44d5db301b8daa669433a996471febabd30ab
pavlov_i_a <pavlov2.ia@edu.spbstu.ru>
2024-11-07 20:06:25 +0300

    Download Files

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-from pydantic import BaseModel
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import os
 
 import uvicorn
+
+app = FastAPI()
+
+dir = "Files"
+
+@app.get("/count")
+async def Count():
+    return str(len(os.listdir(dir)))
+
+@app.get("/download")
+async def download(num : int = 0):
+    if num < 0 or num >= len(os.listdir(dir)):
+        return "Укажите другое количество файлов"
+    return FileResponse(f"{dir}\\{os.listdir(dir)[num]}", filename=os.listdir(dir)[num])
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app,
+                host='127.0.0.1',
+                port=80)
